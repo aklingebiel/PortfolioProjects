0b58a8ddc712032145f6c5749259aff3802be8a8
Aaron <117556075+aklingebiel@users.noreply.github.com>
2025-12-08 19:09:48 -0500

    Enhance SQL to include avg points and wins by era
    Added a new query to calculate average points and wins by conference and era.

--- Day_3.sql
+++ Day_3.sql
@@ -2,7 +2,7 @@ select
 nfl_metrics.team,
 round(avg(nfl_metrics.wins), 2) as avg_wins,
 round(avg(nfl_metrics.losses), 2) as avg_losses,
-round(avg(nfl_metrics.points), 2) as avg_points,
+round(avg(nfl_metrics.points), 2) as avg_points_for,
 round(avg(nfl_metrics.points_opp), 2) as avg_points_against,
 round(avg(nfl_metrics.points-nfl_metrics.points_opp), 2) as avg_point_diff,
 team_meta.conference,
@@ -17,3 +17,20 @@ from nfl_metrics
 left outer join team_meta on team_meta.team = nfl_metrics.team
 group by nfl_metrics.team, conference, division, era
 order by conference, division;
+
+select 
+round(avg(nfl_metrics.points), 2) as avg_points_for,
+round(avg(nfl_metrics.points_opp), 2) as avg_points_against,
+round(avg(nfl_metrics.points-nfl_metrics.points_opp), 2) as avg_point_diff, 
+round(avg(nfl_metrics.wins), 2) as avg_wins,
+team_meta.conference,
+CASE
+	when year >= 2003 and year <= 2009 then '2000s'
+    when year >= 2010 and year <= 2019 then '2010s'
+    when year >= 2020 and year <= 2029 then '2020s'
+    else 'UNKNOWN'
+end as era
+from nfl_metrics
+left outer join team_meta on team_meta.team = nfl_metrics.team
+group by team_meta.conference, era
+order by era, avg_wins desc;
