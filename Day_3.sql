1667e57faa35b483856b2ac7f413f6416a8735a8
Aaron <117556075+aklingebiel@users.noreply.github.com>
2025-12-08 20:41:03 -0500

    Day_3.sql

--- Day_3.sql
+++ Day_3.sql
@@ -19,38 +19,64 @@ update nfl
 set team = 'Las Vegas Raiders'
 where team in ('Oakland Raiders')
 
-select
-round(avg(nfl_metrics.wins), 2) as avg_wins,
-round(avg(nfl_metrics.losses), 2) as avg_losses,
-round(avg(nfl_metrics.points), 2) as avg_points_for,
-round(avg(nfl_metrics.points_opp), 2) as avg_points_against,
-round(avg(nfl_metrics.points-nfl_metrics.points_opp), 2) as avg_point_diff,
-team_meta.conference,
-team_meta.division,
-CASE
-	when year >= 2003 and year <= 2009 then '2000s'
-    when year >= 2010 and year <= 2019 then '2010s'
-    when year >= 2020 and year <= 2029 then '2020s'
+with division_season as (
+  select 
+  team_meta.conference,
+  team_meta.division,
+  	CASE
+		when year >= 2003 and year <= 2009 then '2000s'
+   		when year >= 2010 and year <= 2019 then '2010s'
+    	when year >= 2020 and year <= 2029 then '2020s'
     else 'UNKNOWN'
-end as era
-from nfl_metrics
-left outer join team_meta on team_meta.team = nfl_metrics.team
-group by nfl_metrics.team, conference, division, era
-order by conference, division;
+  	end as era,
+  nfl_metrics.year,
+  sum(nfl_metrics.points) as total_points_for,
+  sum(nfl_metrics.points_opp) as total_points_against,
+  sum(nfl_metrics.wins) as total_wins,
+  sum(nfl_metrics.losses) as total_losses
+  from nfl_metrics
+  left outer join team_meta on team_meta.team = nfl_metrics.team
+  group by team_meta.conference, team_meta.division, era, year
+)
 
 select 
-round(avg(nfl_metrics.points), 2) as avg_points_for,
-round(avg(nfl_metrics.points_opp), 2) as avg_points_against,
-round(avg(nfl_metrics.points-nfl_metrics.points_opp), 2) as avg_point_diff, 
-round(avg(nfl_metrics.wins), 2) as avg_wins,
-team_meta.conference,
-CASE
-	when year >= 2003 and year <= 2009 then '2000s'
-    when year >= 2010 and year <= 2019 then '2010s'
-    when year >= 2020 and year <= 2029 then '2020s'
+conference,
+division,
+round(avg(total_points_for), 2),
+round(avg(total_points_against), 2),
+round(avg(total_wins), 2),
+round(avg(total_losses), 2),
+era
+from division_season
+group by conference, division, era
+order by conference, division, era;
+
+with conference_season as (
+  select 
+  team_meta.conference,
+  	CASE
+		when year >= 2003 and year <= 2009 then '2000s'
+   		when year >= 2010 and year <= 2019 then '2010s'
+    	when year >= 2020 and year <= 2029 then '2020s'
     else 'UNKNOWN'
-end as era
-from nfl_metrics
-left outer join team_meta on team_meta.team = nfl_metrics.team
-group by team_meta.conference, era
-order by era, avg_wins desc;
+  	end as era,
+  nfl_metrics.year,
+  sum(nfl_metrics.wins) as total_wins,
+  sum(nfl_metrics.losses) as total_losses,
+  sum(nfl_metrics.points) as total_points,
+  sum(nfl_metrics.points_opp) as total_points_against
+  from nfl_metrics
+  left OUTER join team_meta on team_meta.team = nfl_metrics.team
+  group by conference, era, year
+)
+
+select 
+conference,
+round(avg(total_points), 2) as avg_points_for,
+round(avg(total_points_against), 2) as avg_points_against,
+round(avg(total_wins), 2) as avg_wins,
+round(avg(total_losses), 2) as avg_losses,
+era
+from conference_season
+group by conference, era
+order by era;
